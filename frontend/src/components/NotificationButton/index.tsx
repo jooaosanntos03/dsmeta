3bc306027d6a5555030d5b2803f74384d7355656
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2022-09-06 17:44:45 -0300

    Primeiro componente

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from "../../assets/img/icone-notificacao.svg"
+import "./style.css"
+
+function NotificationButton() {
+    return (
+
+        <div className="dsmeta-card-notificacao">
+            <img src={icon} alt="Ícone de notificação"/>
+        </div>
+
+    )
+}
+
+export default NotificationButton
