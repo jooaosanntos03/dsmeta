0e43269b05b1c8a9822d9da8764a1867b2a1db29
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2022-09-07 17:56:45 -0300

    Implementação da busca por data

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -15,8 +19,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findSales() {
-		return this.saleRepository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.trim().equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.trim().equals("") ? today : LocalDate.parse(maxDate);
+		
+		return this.saleRepository.findSales(min, max, pageable); 
 	}
 	
 }
